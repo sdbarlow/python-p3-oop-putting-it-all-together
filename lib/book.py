41900db50eedca573895b373d8613545f69b078c
sdbarlow <sethdbarlow@gmail.com>
2023-03-16 12:09:53 -0600

    all tests pass

--- lib/book.py
+++ lib/book.py
@@ -1,4 +1,23 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title, page_count = 0):
+        self.title = title
+        self.page_count = page_count
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+    def get_count(self):
+        print("getting page count")
+        return self._count
+    def set_count(self, count):
+        if isinstance(count, int):
+            print("setting page count")
+            self._count = count
+        else:
+            print("page_count must be an integer")
+    page_count = property(get_count, set_count)
+
+
+sethsbook = Book("green eggs and ham", 50)
+sethsbook.turn_page()
+
